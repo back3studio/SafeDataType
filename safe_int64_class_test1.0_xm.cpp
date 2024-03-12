cd06b4393742b69b54fe54596855e42fdbb51db4
Type-Wyxdxm <162795823+Type-Wyxdxm@users.noreply.github.com>
2024-03-12 22:23:53 +0800

    Create safe_int64_class_test1.0_xm.cpp
    Quite a big change! We use class instead of struct!
    It is also an exercise for GESP test in Shanghai on Mar.16.
    In fact, I did too little exercise for it!
    Have a good day!

---
+++ safe_int64_class_test1.0_xm.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+class safe_int64{
+public:
+	safe_int64(){ 
+		flowed=0;
+		val=0;
+	}
+	~safe_int64(){}
+	
+	safe_int64 operator+(safe_int64 sll){
+		safe_int64 ans;
+		char addi=0;
+		for(int i=0;i<63;i++){
+			char tmp=val[i]+sll.val[i]+addi;
+			ans.val[i] = tmp%2;
+			addi = tmp/2;
+		}
+		if(addi){
+			ans.val.set();
+			ans.val[63]=0;
+			ans.flowed=1;
+		}
+		return ans;
+	} 
+	safe_int64 operator*(safe_int64 sll){
+		safe_int64 ans;
+		for(int i=0;i<63;i++){
+			safe_int64 t;
+			if(sll.val[i]){
+				int tmp=val[63];
+				t.val = val<<i;
+				t.val[63]=tmp;
+				ans = ans+t;
+				if(ans.flowed){
+					ans.val.set();
+					ans.val[63]=0;
+					return ans;
+				}
+			}
+		}
+		return ans;
+	}
+	
+	void in(){
+		scanf("%lld",&val);
+		return;
+	}
+	void out_d(bool enter=0){
+		printf("%lld",val);
+		if(enter){
+			printf("\n");
+		}
+		return;
+	}
+	void out_b(bool enter=0){
+		for(int i=63;i>=0;i--){
+			cout << val[i];
+		}
+		if(enter){
+			cout << endl;
+		}
+		return;
+	}
+private:
+	bitset<64> val;
+	bool flowed;
+};
+
+// try to use si64
+safe_int64 a,b,c;
+int main(){
+	a.in();
+	a.out_d(1);
+	a.out_b(1);
+	b.in();
+	c=a*b;
+	c.out_d(1);
+	c.out_b(1);
+}
